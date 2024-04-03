59cb96a696e85a054bb06252f9093a047f79377f
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-04-03 15:16:34 -0400

    added logic for handling and maintaining prompt context

--- src/api/utils/openai/calls.py
+++ src/api/utils/openai/calls.py
@@ -3,37 +3,50 @@ from src.api.utils.openai.config import get_openai_key
 from src.api.utils.openai.prompts import scaffold_response_prompt, continue_conversation_prompt
 
 
-def scaffold_response_call( messages, grade_level, academic_topic):
-    """ Takes in a question. Returns an outline scaffolding the concepts building
-     to the concepet's answer."""
+def scaffold_response_call( client_messages, prompt_messages, grade_level, academic_topic):
+    """ Takes in a question. Generates an outline scaffolding the concepts building
+     to the concepet's answer. Returns the raw assistant response, updates the prompt_mesages
+     list, and returns the list"""
 
     api_key = get_openai_key()
     openai.api_key = api_key
 
-    new_message = scaffold_response_prompt(messages, grade_level, academic_topic)
+    new_prompt_message = scaffold_response_prompt(client_messages, grade_level, academic_topic)
+    prompt_messages = prompt_messages + [new_prompt_message]
 
     chat_completion = openai.chat.completions.create(
-        messages=messages + [new_message],
+        messages=prompt_messages + [new_prompt_message],
         model="gpt-3.5-turbo",
         # model="gpt-4-0125-preview",
     )
 
-    return chat_completion.choices[0].message.content
+    chat_completion_content = chat_completion.choices[-1].message.content
+    prompt_message_object = {"role": "assistant", "content": chat_completion_content}
+    prompt_messages = prompt_messages + [prompt_message_object]
 
-def continue_conversation_call(messages):
-    """ Takes in a question. Returns an outline scaffolding the concepts building
-     to the concepet's answer."""
+    return {'updated_prompt_messages': prompt_messages, 
+            "client_message": chat_completion_content}
+
+def continue_conversation_call(client_messages, prompt_messages):
+    """ Continues the conversation while maintaining prompt context. Returns the 
+    raw assistant response and the updated prompt messages list"""
 
     api_key = get_openai_key()
     openai.api_key = api_key
 
-    new_message = continue_conversation_prompt(messages)
-    print("here's the new_message :", new_message)
+    new_prompt_message = continue_conversation_prompt(client_messages)
+    prompt_messages = prompt_messages + [new_prompt_message]
+    print("here's the new_prompt_message :", new_prompt_message)
 
     chat_completion = openai.chat.completions.create(
-        messages=messages + [new_message],
+        messages=prompt_messages,
         model="gpt-3.5-turbo",
         # model="gpt-4-0125-preview",
     )
 
-    return chat_completion.choices[-1].message.content
+    chat_completion_content = chat_completion.choices[-1].message.content
+    prompt_message_object = {"role": "assistant", "content": chat_completion_content}
+    prompt_messages = prompt_messages + [prompt_message_object]
+
+    return {'updated_prompt_messages': prompt_messages, 
+            "client_message": chat_completion_content}

2780e6d9961dc904c065358b0d120770b60c0759
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-04-03 15:13:56 -0400

    updated to handle prompt_messages

--- src/api/utils/orchestration.py
+++ src/api/utils/orchestration.py
@@ -1,14 +1,12 @@
 from src.api.utils.openai.calls import scaffold_response_call, continue_conversation_call
 
-def orchestrate_chatbot_pipeline(messages, grade_level, academic_topic):
+def orchestrate_chatbot_pipeline(client_messages, prompt_messages, grade_level, academic_topic):
     """Orchestrates flow of data across prompts and calls."""
     
-    print("messages[0]['role']: ", messages[0]["role"])
-    if len(messages) == 1 and messages[0]["role"] == "user":
-        print("scaffold_response_call")
-        response = scaffold_response_call(messages=messages, grade_level=grade_level, academic_topic=academic_topic)
+    print("client_messages[0]['role']: ", client_messages[0]["role"])
+    if len(client_messages) == 1 and client_messages[0]["role"] == "user":
+        response = scaffold_response_call(client_messages=client_messages, prompt_messages=prompt_messages, grade_level=grade_level, academic_topic=academic_topic)
     else:
-        print("i'm about to do continue_conversation_prompt")
-        response = continue_conversation_call(messages=messages)
+        response = continue_conversation_call(client_messages=client_messages, prompt_messages=prompt_messages)
 
     return response

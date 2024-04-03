266dae1a376490f4f62f75688d9547c3271d3781
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-04-03 15:12:29 -0400

    updated logic to accept prompt_messages and return prompt_messages

--- src/api/filters/views.py
+++ src/api/filters/views.py
@@ -11,21 +11,25 @@ class Filter(Resource):
     @filters_namespace.expect(messages_model, validate=True)
     def post(self):
         """
-        Receives list of messages and context (grade level, academic topic). 
+        Receives list of client messages, prompt messages, and 
+        context (grade level, academic topic). Client messages are strictly for client
+        display. prompt messages are to maintain conversational context. 
         Orchestrates processing of chat conversation, returning the resulting
-        message. 
+        message and the list of prompt messages with a new user and a new assistant
+        dictionary appended. 
         """
         post_data = request.get_json()
 
-        messages = post_data.get("messages")        
-        print("messages :", messages)
+        client_messages = post_data.get("clientMessages")
+        prompt_messages = post_data.get("promptMessages")        
         grade_level = post_data.get("gradeLevel")       
         academic_topic = post_data.get("academicTopic")
 
         try:
-            updated_messages = orchestrate_chatbot_pipeline(messages=messages, grade_level=grade_level, academic_topic=academic_topic)
-            print('updated_message: ', updated_messages)
-            return updated_messages, 200
+            updated_messages = orchestrate_chatbot_pipeline(client_messages=client_messages, prompt_messages=prompt_messages, grade_level=grade_level, academic_topic=academic_topic)
+            client_message = updated_messages['client_message']
+            updated_prompt_messages = updated_messages['updated_prompt_messages']
+            return {"clientMessage": client_message, "promptMessages": updated_prompt_messages}, 200
         except Exception as e:
             return {"error": str(e)}, 500
 
